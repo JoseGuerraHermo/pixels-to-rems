62c36a2c156c876029cafc5a7e9b4d62ab37990f
Jose Guerra Hermo <guerrahermo@gmail.com>
2022-05-01 19:56:11 +0100

    fix github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 
-const Footer = () => (
-  <div className="h-6 w-full flex justify-end">
-    <p 
-      className="mr-2 text-xs text-orange-400"
-    >
-      Github: 
-        <a 
-          className="no-underline hover:underline" 
-          href="https://github.com/JoseGuerraHermo/pixels-to-rems"
-        >
-          pixels-to-rems
-        </a>
-    </p>
-  </div>
-);
+const Footer = () => {
+  const gitHub = "https://github.com/JoseGuerraHermo/pixels-to-rems"
+  const goToGitHub = () => window.open(gitHub, "_blank");
+  
+  return (
+    <div className="h-6 w-full flex justify-end">
+      <p 
+        className="mr-2 text-xs text-orange-400"
+      >
+        Github: 
+          <a 
+            className="no-underline hover:underline" 
+            href={gitHub}
+            onClick={goToGitHub}
+          >
+            pixels-to-rems
+          </a>
+      </p>
+    </div>
+  )
+};
 
 export default Footer;
